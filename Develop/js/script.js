2fed2837fc4ddb9b39865bf0a5dcd18d4ce30331
Joshua Sands <josh.sands@mail.com>
2021-06-12 18:21:07 -0500

    add local storage save feature, adjusted resolution adaptation & added README

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,3 +1,18 @@
+// CRITERIA
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+// WHEN I click into a timeblock
+// THEN I can enter an event
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
+
 // display the current date and time
 $("#currentDay").text(moment().format("LLLL"));
 
@@ -16,41 +31,57 @@ hourArray = [
   "2PM",
   "3PM",
   "4PM",
-  "5PM"
+  "5PM",
 ];
 
 idArray = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
 
-// Create rows for each hour
-var hourRows = function () {
-  var currentTime = moment().hour();
+var currentTime = moment().hour();
 
-  for (var i = 0; i < hourArray.length; i++) {
-    var row = $("<div>").addClass("row time-block").attr("id", idArray[i]);
+// Create rows with for loop for each hour slot
+for (var i = 0; i < hourArray.length; i++) {
+  var row = $("<div>").addClass("row time-block").attr("id", idArray[i]);
 
-    if (idArray[i] < currentTime) {
-      $(row).removeClass("future");
-      $(row).removeClass("present");
-      $(row).addClass("past");
-    } else if (idArray[i] === currentTime) {
-      $(row).removeClass("past");
-      $(row).removeClass("future");
-      $(row).addClass("present");
-    } else {
-      $(row).removeClass("present");
-      $(row).removeClass("past");
-      $(row).addClass("future");
-    }
-
-    var hour = $("<div>").addClass("col hour").text(hourArray[i]);
-    var textArea = $("<textarea>").addClass("col-10 description");
-    var saveButton = $("<button>").addClass("btn saveBtn col");
-    var saveIcon = $("<i>").addClass("fas fa-save");
-
-    $(".container").append(row);
-    row.append(hour, textArea, saveButton, saveIcon);
-    saveButton.append(saveIcon);
+  // for each row check current hour and display appropriate color
+  if (idArray[i] < currentTime) {
+    $(row).removeClass("future");
+    $(row).removeClass("present");
+    $(row).addClass("past");
+  } else if (idArray[i] === currentTime) {
+    $(row).removeClass("past");
+    $(row).removeClass("future");
+    $(row).addClass("present");
+  } else {
+    $(row).removeClass("present");
+    $(row).removeClass("past");
+    $(row).addClass("future");
   }
-};
 
-hourRows();
+  var hour = $("<div>").addClass("col hour").text(hourArray[i]);
+
+  // creates textarea with corresponding localStorage
+  var textArea = $("<textarea>")
+    .addClass("col-10 description")
+    .html(localStorage.getItem(idArray[i]));
+
+  var saveButton = $("<button>").addClass("btn saveBtn col");
+
+  var saveIcon = $("<i>").addClass("fas fa-save");
+
+  $(".container").append(row);
+  row.append(hour, textArea, saveButton, saveIcon);
+  saveButton.append(saveIcon);
+}
+
+// Wait for rows to append before calling new dom elements
+$(document).ready(function () {
+  // saveBtn click listener
+  $(".saveBtn").on("click", function () {
+    // Get nearby values of the description in JQuery
+    var noteSave = $(this).siblings(".description").val();
+    var hourSave = $(this).parent().attr("id");
+
+    // Save text to local storage
+    localStorage.setItem(hourSave, noteSave);
+  });
+});
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # work-day-scheduler
+
+## Purpose
+Save hourly notes during the work day
+
+## Built With
+* HTML
+* CSS
+* JavaScript
+
+## Website
+https://joshsands.github.io/work-day-scheduler/
+
+## GitHub Repository
+https://github.com/Joshsands/work-day-scheduler
+
+## Contribution
+Made by Joshua Sands
+
+## Screenshot
+<img src="./Assets/screenshot.png" />
